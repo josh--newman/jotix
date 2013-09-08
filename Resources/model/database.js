6bd924d59769d6163084f61e761f10a84af8b154
Josh Newman <josh@Joshs-MacBook-Pro.local>
2013-09-08 16:42:40 +1000

    Updated database functions

--- Resources/model/database.js
+++ Resources/model/database.js
@@ -3,11 +3,13 @@ function Database(_args) {
 	// create a new note
 	function createNote(parentId, content) {
 		var db = Ti.Database.open("jotixdbv1");
+		var index = getNextIndex(parentId, db);
 		var breadcrumbs = db.execute('SELECT breadcrumbs FROM notes WHERE noteId = (?)', parentId).fieldByName('breadcrumbs') + ">" + content;
-		db.execute('INSERT INTO notes (parentId, content, breadcrumbs) VALUES (?,?,?)', parentId, content, breadcrumbs);
+		db.execute('INSERT INTO notes (parentId, content, index, breadcrumbs) VALUES (?,?,?,?)', parentId, content, index, breadcrumbs);
 		db.close();
 	}
 	
+	// load notes
 	function showNotes(parentId) {
 		var db = Ti.Database.open("jotixdbv1");
 		var rows = db.execute('SELECT * FROM notes WHERE parentId = (?)', parentId);
@@ -27,6 +29,19 @@ function Database(_args) {
 		var db = Ti.Database.open("jotixdbv1");
 		db.execute('DELETE FROM notes WHERE noteId = ' + id);
 		db.close();
+	}
+	
+	// helper methods
+	function getNextIndex(parentId, db) {
+		var index = db.execute('SELECT [index] FROM notes WHERE parentId = (?) ORDER BY [index] ASC', parentId);
+		var max = 0;
+		while (index.isValidRow()) {
+			max = index.fieldByName('index') > max ? index.fieldByName('index') : max;
+			index.next();
+		}
+		index.close();
+		index = ++max;
+		return index;
 	}
 	
 }
