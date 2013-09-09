8a95a7c2cdcada303549992e871fe6e744b2bb6d
Smiley <rjchatfield@gmail.com>
2013-09-09 14:35:40 +1000

    Added Open In Safari prompt in settings/developer page

--- Resources/ui/settings/SettingsWeb.js
+++ Resources/ui/settings/SettingsWeb.js
@@ -7,9 +7,32 @@ function SettingsWeb(_args) {
 		tabBarHidden: true
 	});
 	
-	self.add(Ti.UI.createWebView({
+	var webview = Ti.UI.createWebView({
 		url: 'http://www.apple.com'
-	}));
+	});
+	self.add(webview);
+	
+	var openInSafariButton = Ti.UI.createButton({systemButton:Titanium.UI.iPhone.SystemButton.ACTION});
+	self.setRightNavButton(openInSafariButton);
+	
+	openInSafariButton.addEventListener('click', function(e){
+	  var dialog = Ti.UI.createAlertDialog({
+	    cancel: 0,
+	    open: 1,
+	    buttonNames: ['Cancel', 'Open'],
+	    message: 'Open in Safari?',
+	    title: 'Safari'
+	  });
+	  dialog.addEventListener('click', function(e){
+	    if (e.index === e.source.cancel){
+	    	Ti.API.info('The cancel button was clicked');
+	    } else if (e.index === e.source.open) {
+	    	Titanium.Platform.openURL(webview.url);
+		}
+		dialog.close();
+	  });
+	  dialog.show();
+	});
 	
 	return self;
 }
