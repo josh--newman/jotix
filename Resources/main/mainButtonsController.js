77c7750428161fc320b01a7b0d4374a2972c44d3
Smiley <rjchatfield@gmail.com>
2013-09-17 20:48:20 +1000

    Added dummy OptionDialogs to share and sort buttons

--- Resources/main/mainButtonsController.js
+++ Resources/main/mainButtonsController.js
@@ -7,3 +7,21 @@
 searchButton.addEventListener('click', function() {
 	searchView.open({modal: true});
 });
+
+actionButton.addEventListener('click', function() {
+	Ti.UI.createOptionDialog({
+		       cancel: 1,
+		      options: ['Email', 'Cancel'],
+		selectedIndex: 0,
+		        title: 'Share?'
+	}).show();
+});
+
+sortButton.addEventListener('click', function() {
+	Ti.UI.createOptionDialog({
+		       cancel: 3,
+		      options: ['Manually', 'By Content', 'By Date', 'Cancel'],
+		selectedIndex: 0,
+		        title: 'Sort notes'
+	}).show();
+});
